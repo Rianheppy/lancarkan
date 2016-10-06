fbfc6a825f1c73c3aa5393347a96fd0301f2d27c
Rianheppy <lihatdisiniweb@gmail.com>
2016-10-06 19:29:13 +0700

    Add files via upload

---
+++ scrip-posting-shaflink.js
@@ -0,0 +1,9 @@
+<center>
+<h2 id="daplong" style="color: daplong;">
+Please Wait...</h2>
+<script src="Kode_JavaScript" type="text/javascript"></script> <script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://cardeal.lihatdisini.com/2016/10/post-1.html?url=", "");
+document.write('<button id="download" class="Visit_Link" onclick="changeLink();" style="display: none;text-align:justify;margin-bottom:2px;">Visit Link</button>')
+</script></center>
